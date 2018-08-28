0ab0852cb8ce354c9e0fd1bca8cc042584844dc1
jeffin <jeffin@hifx.co.in>
2018-08-28 16:25:01 +0530

    vote complted

--- lucks/views.py
+++ lucks/views.py
@@ -21,7 +21,7 @@ from . import models
 
 @login_required(login_url='/login/')
 def index(request):
-    lucks = models.Luck.objects.all()
+    lucks = models.Luck.objects.filter(status='open')
     return render(request, "lucks/index.html", {'lucks':lucks})
 
 
@@ -43,7 +43,10 @@ def lucks_view(request,l_id):
 
     l = models.Luck.objects.get(pk=l_id)
     comments = models.Comment.objects.filter(luck=l).order_by('-id')[:5]
-    return render(request, "lucks/view.html", {'luck':l,'comments':comments})
+    h_count = models.Like.objects.filter(luck=l,user=request.user,option='head').count()
+    t_count = models.Like.objects.filter(luck=l,user=request.user,option='tail').count()
+
+    return render(request, "lucks/view.html", {'luck':l,'comments':comments,'h_count':h_count,'t_count':t_count})
 
 @login_required(login_url='/login/')
 def lucks_del(request,l_id):
@@ -99,14 +102,13 @@ def lucks_add_new(request):
 def addcomment(request):
     if request.method == "POST":
         comment = request.POST.get('the_comment')
-        print request.POST.keys()
-        print comment
+
         response_data = {}
         try:
             l = models.Luck(pk=request.POST['luck'])
             c = models.Comment(comment=comment, user=request.user,luck=l)
             c.save()
-            print request.user.username
+
             response_data['result'] = 'success!'
             response_data['html'] = """
                 <strong class="pull-left primary-font">"""+request.user.username+"""</strong>
@@ -137,14 +139,20 @@ def addcomment(request):
 @login_required(login_url='/login/')
 def like(request,l_id):
     if request.user and request.method == "GET":
+        vote = request.GET['v']
         l = models.Luck.objects.get(id=l_id)
         w = Walet.objects.get(user=request.user)
+
         if l.amount < w.balance:
             w.balance -= l.amount
+
+            like = models.Like(user=request.user,luck=l,option=vote)
+            like.save()
+
             w.save()
-            
+
             messages.success(request, "Successfully chosed")
-            return HttpResponseRedirect("/wallet/")
+            return HttpResponseRedirect("/lucks/view/"+l_id+"/")
 
         else:
             messages.warning(request, "You don't have enough balance. Please add balance")
